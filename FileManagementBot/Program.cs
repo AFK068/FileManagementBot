3d815f6285ad8377b238c20d14ecad938311f0fd
Ivan <93443694+AFK068@users.noreply.github.com>
2024-04-16 13:05:20 +0300

    Update Program.cs

--- FileManagementBot/Program.cs
+++ FileManagementBot/Program.cs
@@ -9,9 +9,7 @@ class Program
     /// The entry point method for the application.
     /// </summary>
     static void Main()
-    {
-        // https://t.me/FileManagmentByAfk_bot
-        
+    {   
         TelegramBotLogics telegramBotLogics = new TelegramBotLogics();
         
         Logging logging = new Logging();
